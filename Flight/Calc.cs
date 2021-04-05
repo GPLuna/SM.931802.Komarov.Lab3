5d2a990149d1e2b57a035b1fa8c06243aa7d4337
GPLuna <gpluna@mail.ru>
2021-04-05 20:36:45 +0700

    Добавьте файлы проекта.

---
+++ Flight/Calc.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flight
+{
+	class Calc
+	{
+        const double dt = 0.01;
+        const double g = 9.81;
+        public double a;
+        public double v0;
+        public double y0;
+        public double S;
+        public double m;
+        public double k;
+        public double t;
+        public double x;
+        public double y;
+        public double vx;
+        public double vy;
+        
+        public void initialization()
+        {
+            vx = v0 * Math.Cos(a * Math.PI / 180);
+            vy = v0 * Math.Sin(a * Math.PI / 180);
+        }
+        public double[] getWeightHeight()
+        {
+            double vxx = vx;
+            double vyy = vy;
+            double xx = x;
+            double yy = y;
+            double ymax = 0;
+            double[] arr = new double[2];
+
+            while (yy >= 0)
+            {
+                vxx = vxx - k * vxx * Math.Sqrt(vxx * vxx + vyy * vyy) * dt;
+                vyy = vyy - (g + k * vyy * Math.Sqrt(vxx * vxx + vyy * vyy)) * dt;
+
+                xx = xx + vxx * dt;
+                yy = yy + vyy * dt;
+
+                if (yy > ymax) ymax = yy;
+            }
+            arr[0] = xx;
+            arr[1] = ymax;
+
+            return arr;
+        }
+        public void FindingXY()
+        {
+            vx = vx - k * vx * Math.Sqrt(vx * vx + vy * vy) * dt;
+            vy = vy - (g + k * vy * Math.Sqrt(vx * vx + vy * vy)) * dt;
+
+            x = x + vx * dt;
+            y = y + vy * dt;
+        }
+        public Tuple<double, double, double> GetSize()
+        {
+            double sina = Math.Sin(a * Math.PI / 180);
+            double ymax = y0 + (v0 * v0 * sina * sina / 2 / g);
+            double tpol = (v0 * sina + Math.Sqrt(v0 * v0 * sina * sina + 2 * g * y0)) / g;
+            double xmax = v0 * tpol * Math.Cos(a * Math.PI / 180);
+            return Tuple.Create(xmax, ymax, tpol);
+        }
+    }
+}
---
+++ Flight/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flight
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const double C = 0.15;
+        const double rho = 1.29;
+        const double dt = 0.01;
+        Calc n = new Calc();
+        
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            n.a = (double)edAngle.Value;
+            n.v0 = (double)edSpeed.Value;
+            n.y0 = (double)edHeight.Value;
+            n.m = (double)edWeight.Value;
+            n.S = (double)edSquare.Value;
+
+            var sizes = n.GetSize();
+            var size1 = Convert.ToInt32(sizes.Item1) + 1;
+            var size2 = Convert.ToInt32(sizes.Item2) + 1;
+            chart1.ChartAreas[0].AxisX.Maximum = size1;
+            chart1.ChartAreas[0].AxisY.Maximum = size2;
+
+            n.t = 0;
+            n.x = 0;
+            n.y = n.y0;
+
+            n.initialization();
+            n.k = 0.5 * C * n.S * rho / n.m;
+
+            chart1.Series[0].Points.Clear();
+            chart1.Series[0].Points.AddXY(n.x, n.y);
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            n.FindingXY();
+            n.t += dt;
+            chart1.Series[0].Points.AddXY(n.x, n.y);
+            time.Text = "Время: " + Convert.ToString(n.t);
+            if (n.y <= 0) timer1.Stop();
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled) timer1.Stop();
+            else timer1.Start();
+        }
+    }
+}
